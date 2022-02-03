bf734683ec8b6a03396b988d0b99cfe6c1d3416e
Dalton <slattery.da@gmail.com>
2022-02-03 10:20:05 -0600

    clear errors

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,24 +1,14 @@
 import React, {useState} from 'react';
 
 function Comments({comments}) {
-
-  
-    const displayedComments = comments.map((comment) =>  {const [upVotes, setUpVotes] = useState(comment.upvotes)
-    const [downVotes, setdownVotes] = useState(comment.downvotes)
-    return (
-    <div key={comment.id}>
-        <h3>{comment.user}</h3>
-        <p>{comment.comment}</p>
-        <button onClick={handleUpVotes}>{upVotes} 👍</button>
-    </div>)}
-
+    
     const commentCount = comments.length
-    
-    
-    function handleUpVotes(){setUpVotes(upVotes+1)}
-    // function handleDownVotes(){setdownVotes(downVotes+1)}
-
-
+    const displayedComments = comments.map((comment) =>  {
+        return (
+        <div key={comment.id}>
+            <h3>{comment.user}</h3>
+            <p>{comment.comment}</p>
+        </div>)})
 
     return(
         <>
